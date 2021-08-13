0608eed7a690a6465fcb189572c186b67084f463
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-08-13 21:57:08 +0200

    0-create_a_file added

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# creates a file in the tmp diirectory of 
+# the node 
+
+file { 'holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
