08396e58c368740367539a0294ac4e3728d9841f
ollyimanishimwe <imanishimweolyy@gmail.com>
2021-08-23 20:39:11 +0200

    2-puppet_custom_http_response_header added

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to nginx on LXD container web1!</title>
+<style>
+  body {
+      width: 35em;
+      margin: 0 auto;
+      font-family: Tahoma, Verdana, Arial, sans-serif;
+  }
+</style>
+</head>
+<body>
+<h1>Welcome to nginx on LXD container web1!</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
